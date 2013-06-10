a2dde082a56341145574c42dde74dcaf178a3bad
Bill Nicholson <GitHub@NicholsonComputerConsulting.com>
2013-06-10 16:41:02 -0400

    AnotherBranch

--- InterfaceDemoMultipleInheritance/src/MainPackage/Main.java
+++ InterfaceDemoMultipleInheritance/src/MainPackage/Main.java
@@ -1,5 +1,6 @@
 /* Cool Stuff */
 // Added a comment on 6/10/2013 to see if I could puch it upstream.
+// This comment should only be in the branch called AnotherBranch
 package MainPackage;
 
 public class Main {
